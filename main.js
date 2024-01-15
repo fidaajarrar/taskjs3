fdebd96ff2df2fe011042999fa8fb4d232e8c0cb
fidaajarrar <fidaa.j152@gmail.com>
2024-01-15 22:49:37 +0200

    jstask3

---
+++ main.js
@@ -0,0 +1,11 @@
+var registerForm = document.querySelector(".registerForm");
+var users = [];
+ registerForm.onsubmit = function(e){
+    e.preventDefault();
+    var elements = e.target.elements
+    var user= {
+
+        
+    }
+    
+ }
